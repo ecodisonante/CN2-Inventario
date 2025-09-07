1b3850b9182198ef2190baedf7e35ce94bb7a640
Francisco Valdes <ecodisonante@gmail.com>
2025-09-07 02:46:26 -0300

    feat: fix  exception throwments

--- functions/src/main/java/com/inventario/util/Json.java
+++ functions/src/main/java/com/inventario/util/Json.java
@@ -1,6 +1,6 @@
-// util/Json.java
 package com.inventario.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.NoArgsConstructor;
@@ -11,11 +11,11 @@ public final class Json {
 
   private static final ObjectMapper MAPPER = new ObjectMapper();
 
-  public static <T> T read(String body, Class<T> type) throws Exception {
+  public static <T> T read(String body, Class<T> type) throws JsonProcessingException {
     return MAPPER.readValue(body, type);
   }
 
-  public static String write(Object obj) throws Exception {
+  public static String write(Object obj) throws JsonProcessingException {
     return MAPPER.writeValueAsString(obj);
   }
 }
